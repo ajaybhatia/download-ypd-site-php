a202a97f96bee5ac3b691fc955e0fbc48b88e53f
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-06-07 20:44:00 +0530

    Static page ready

--- index.php
+++ index.php
@@ -1,34 +1,193 @@
 <!DOCTYPE html>
-<html lang="en-US">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Downloads | YU Play Dev</title>
-    <!-- Main information -->
-    <link rel="icon" sizes="16x16" href="favicon.ico">
+<html lang="en">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!-- Fontawesome CSS -->
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
+    <head>
 
-    <!-- jQuery -->
-    <script src="assets/js/jquery-2.2.4.min.js"></script>
-    <!-- Bootstrap JavaScript -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="Hello World"></script>
-    <!-- Custom js -->
-    <script src=""></script>
-  </body>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Downloads | YU Play Dev</title>
+
+        <!-- Icon -->
+        <link rel="icon" sizes="16x16" href="favicon.ico">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="assets/css/main.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-blue no-margin">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header fixed-brand">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"  id="menu-toggle">
+                    <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    Yu Play Dev Downloads
+                </a>
+            </div><!-- navbar-header-->
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active" ><button class="navbar-toggle collapse in" data-toggle="collapse" id="menu-toggle-2"> <span class="glyphicon glyphicon-th-large" aria-hidden="true"></span></button></li>
+                </ul>
+            </div><!-- bs-example-navbar-collapse-1 -->
+        </nav>
+        <div id="wrapper">
+            <!-- Sidebar -->
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav nav-pills nav-stacked" id="menu">
+
+                    <li class="active">
+                        <a href="#">
+                            <span class="fa-stack fa-lg pull-left">
+                                <i class="fa fa-mobile fa-stack-2x"></i>
+                            </span>
+                            Yunicorn
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa-stack fa-lg pull-left">
+                                <i class="fa fa-mobile fa-stack-2x"></i>
+                            </span>
+                            Yutopia
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa-stack fa-lg pull-left">
+                                <i class="fa fa-mobile fa-stack-2x"></i>
+                            </span>
+                            Yureka Note
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa-stack fa-lg pull-left">
+                                <i class="fa fa-mobile fa-stack-2x"></i>
+                            </span>
+                            Yunique
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa-stack fa-lg pull-left">
+                                <i class="fa fa-mobile fa-stack-2x"></i>
+                            </span>
+                            Yuphoria
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="fa-stack fa-lg pull-left">
+                                <i class="fa fa-mobile fa-stack-2x"></i>
+                            </span>
+                            Yureka
+                        </a>
+                    </li>
+                </ul>
+            </div><!-- /#sidebar-wrapper -->
+            <!-- Page Content -->
+            <div id="page-content-wrapper">
+                <div class="container-fluid xyz">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <table class="table table-hover table-striped table-condensed">
+                                <thead>
+                                    <tr>
+                                        <th>Device</th>
+                                        <th>Type</th>
+                                        <th>Filename</th>
+                                        <th>sha1</th>
+                                        <th>Date Added</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Yutopia</td>
+                                        <td>nightly</td>
+                                        <td>
+                                            <a>yufastboot-images-lettuce-160518.tar.gz</a>
+                                        </td>
+                                        <td>14647deb1c6140d9098a9205a756f968437a085d</td>
+                                        <td>May 18, 2016 4:35:00 PM</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Yutopia</td>
+                                        <td>nightly</td>
+                                        <td>
+                                            <a>yufastboot-images-lettuce-160518.tar.gz</a>
+                                        </td>
+                                        <td>14647deb1c6140d9098a9205a756f968437a085d</td>
+                                        <td>May 18, 2016 4:35:00 PM</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Yutopia</td>
+                                        <td>nightly</td>
+                                        <td>
+                                            <a>yufastboot-images-lettuce-160518.tar.gz</a>
+                                        </td>
+                                        <td>14647deb1c6140d9098a9205a756f968437a085d</td>
+                                        <td>May 18, 2016 4:35:00 PM</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Yutopia</td>
+                                        <td>nightly</td>
+                                        <td>
+                                            <a>yufastboot-images-lettuce-160518.tar.gz</a>
+                                        </td>
+                                        <td>14647deb1c6140d9098a9205a756f968437a085d</td>
+                                        <td>May 18, 2016 4:35:00 PM</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Yutopia</td>
+                                        <td>nightly</td>
+                                        <td>
+                                            <a>yufastboot-images-lettuce-160518.tar.gz</a>
+                                        </td>
+                                        <td>14647deb1c6140d9098a9205a756f968437a085d</td>
+                                        <td>May 18, 2016 4:35:00 PM</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /#page-content-wrapper -->
+        </div>
+        <!-- /#wrapper -->
+
+        <!-- jQuery -->
+        <script src="assets/js/jquery-2.2.4.min.js"></script>
+
+        <!-- Bootstrap JavaScript -->
+        <script src="assets/js/bootstrap.min.js"></script>
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="Hello World"></script>
+
+        <!-- Custom js -->
+        <script src="assets/js/main.js"></script>
+    </body>
+
 </html>
